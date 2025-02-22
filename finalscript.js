614958c7cd17c661a2a141f8cacb824972376495
Sharkmin <smspamacc@gmail.com>
2025-02-22 07:21:50 +0000

    Update finalscript.js

--- finalscript.js
+++ finalscript.js
@@ -21,10 +21,8 @@ const questions = [
     question: "Q1/10: You approach Juniper Grove, where you have heard magical creatures live. Curious, you...",
     image: "images/1.gif",
     answers: [
-      { type: "T", text: "Strategically observe from a distance, analysing the best way to approach.", scores: { T: 1, F: 0, J: 0, P: 0 } },
-      { type: "F", text: "Gently call out, hoping to make a friendly connection with the creatures.", scores: { T: 0, F: 1, J: 0, P: 0 } },
-      { type: "J", text: "Carefully plan your approach to not disrupt the grove’s harmony.", scores: { T: 0, F: 0, J: 1, P: 0 } },
-      { type: "P", text: "Excitedly wander in without a plan, ready to embrace whatever happens.", scores: { T: 0, F: 0, J: 0, P: 1 } }
+      { type: "E", text: "Excitedly wander in while calling out to the creatures, hoping to make a friendly connection.", scores: { E: 1, I: 0 } },
+      { type: "I", text: "Strategically observe from a distance, analysing the best way to approach the unfamiliar area.", scores: { E: 0, I: 1 } },
     ]
   },
   {
