715ef9f4bf0a0887e711b8ed389c7c0bf59d0851
Sharkmin <smspamacc@gmail.com>
2025-03-04 00:07:30 +0000

    Update finalscript.js

--- finalscript.js
+++ finalscript.js
@@ -229,7 +229,7 @@ function showResult() {
   const resultContainer = document.getElementById("result");
   resultContainer.innerHTML = `
     <p>#Jellycatplushsona</p>
-    <p>You are an ${personalityType}!</p>
+    <p>Welcome to Juniper Grove!</p>
     <img src="${resultImage}" alt="Your personality image" class="result-image" />
     <div class="result-buttons">
       <button id="share-btn">Share</button>
